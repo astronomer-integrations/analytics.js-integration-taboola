d8523163bfaafc0641297d7009f16b894a575071
Mike Perry <mike@astronomer.io>
2016-07-26 10:52:57 -0400

    removed mocha

--- test/index.test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var Analytics = require('analytics.js-core').constructor;
-var integration = require('analytics.js-integration');
-var sandbox = require('clear-env');
-var tester = require('analytics.js-integration-tester');
-var Kenshoo = require('../lib');
-
